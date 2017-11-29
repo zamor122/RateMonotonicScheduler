fb21a02067295f9db3477bbafa6d41cc14c75060
Joshua Smith Laptop <joshner.smith@gmail.com>
2017-11-29 13:03:55 -0800

    - Adding better thread setup

--- main.cpp
+++ main.cpp
@@ -8,14 +8,27 @@
 using namespace std;
 // Rate Monotonic Scheduler
 
-// Basic Attribute for threads. Set later to have different priorities (make a copy of the value)
 
-// 5 Pthreads
+///////////////////////////////////
+// Variables for program execution
+///////////////////////////////////
+
 pthread_attr_t attr0; // For main thread
 pthread_attr_t attr1; // For T1 thread
 pthread_attr_t attr2; // For T2 thread
 pthread_attr_t attr3; // For T3 thread
 pthread_attr_t attr4; // For T4 thread
+
+int runAmntT1 = 1; // Thread 1 runs doWork() 1 time
+int runAmntT2 = 2; // Thread 2 runs doWork() 2 times
+int runAmntT3 = 4; // Thread 3 runs doWork() 4 times
+int runAmntT4 = 16; // Thread 4 runs doWork() 16 times
+
+// Counters for each thread
+int counterT1;
+int counterT2;
+int counterT3;
+int counterT4;
 
 sched_param param0;
 sched_param param1;
@@ -32,8 +45,23 @@ pthread_t T4;
 //CPU
 cpu_set_t cpu;
 
-
+//DoWorkMatrix
 int doWorkMatrix[10][10]; // 10x10 matrix for do work
+
+/////////////////////////////////////////////////
+// Struct: For passing in specific thread info
+/////////////////////////////////////////////////
+struct threadValues {
+    int* runAmount;
+    int* counter;
+};
+
+// Specific struct for this program;
+threadValues tValArr[4]; // 4 Thread value structs. One for each of the four threads
+
+/////////////////////////////
+// Do Work Function
+/////////////////////////////
 
 void doWork() { // Busy work function, multiplies each column of a 10 x 10 matrix (all numbers are 1)
     int total = 1; // total of multiplication
@@ -48,16 +76,69 @@ void doWork() { // Busy work function, multiplies each column of a 10 x 10 matri
     cout << "we are done" << endl;
 }
 
+//////////////////////////////////////
+// Threading Function
+//////////////////////////////////////
+
+void *scheduler(void * param) {
+
+    counterT1 = 0;
+    counterT2 = 0;
+    counterT3 = 0;
+    counterT4 = 0;
+
+    //Put in counters
+    tValArr[0].counter = &counterT1;
+    tValArr[1].counter = &counterT2;
+    tValArr[2].counter = &counterT3;
+    tValArr[3].counter = &counterT4;
+
+    // Put in run time amount
+    tValArr[0].runAmount = &runAmntT1;
+    tValArr[1].runAmount = &runAmntT2;
+    tValArr[2].runAmount = &runAmntT3;
+    tValArr[3].runAmount = &runAmntT4;
+
+    int tid1 = pthread_create(&T1, &attr1, run_thread, (void *) &tValArr[0]);
+    int tid2 = pthread_create(&T2, &attr2, run_thread, (void *) &tValArr[1]);
+    int tid3 = pthread_create(&T3, &attr3, run_thread, (void *) &tValArr[2]);
+    int tid4 = pthread_create(&T4, &attr4, run_thread, (void *) &tValArr[3]);
+
+    pthread_exit(nullptr);
+}
+
+//////////////////////////////////////
+// Threading Function
+//////////////////////////////////////
+
+void *run_thread(void * param) {
+    int doWorkAmnt = (int) param;
+
+    for (int i = 0; i < doWorkAmnt; i++) {
+        doWork();
+    }
+    counterT1++;
+
+    pthread_exit(nullptr);
+}
+
 int main() {
+
+    // Initialize do work matrix
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            doWorkMatrix[i][j] = 1; // Set all matrix values to one
+        }
+    }
     // Set CPU priority to be the same for all threads;
     CPU_ZERO(&cpu);
     CPU_SET(2, &cpu); //  All threads should run on CPU 2
 
-    pthread_attr_init(&attr0);
-    pthread_attr_init(&attr1);
-    pthread_attr_init(&attr2);
-    pthread_attr_init(&attr3);
-    pthread_attr_init(&attr4);
+    pthread_attr_init(&attr0); // Initialize thread attributes
+    pthread_attr_init(&attr1); // Initialize thread attributes
+    pthread_attr_init(&attr2); // Initialize thread attributes
+    pthread_attr_init(&attr3); // Initialize thread attributes
+    pthread_attr_init(&attr4); // Initialize thread attributes
 
     pthread_attr_setschedparam(&attr0, &param0);  // Set thread priority
     pthread_attr_setschedparam(&attr1, &param0);  // Set thread priority
@@ -65,11 +146,11 @@ int main() {
     pthread_attr_setschedparam(&attr3, &param0);  // Set thread priority
     pthread_attr_setschedparam(&attr4, &param0);  // Set thread priority
 
-    pthread_setaffinity_np(schedulerThread, sizeof(&cpu), &cpu);
-    pthread_setaffinity_np(T1, sizeof(&cpu), &cpu);
-    pthread_setaffinity_np(T2, sizeof(&cpu), &cpu);
-    pthread_setaffinity_np(T3, sizeof(&cpu), &cpu);
-    pthread_setaffinity_np(T4, sizeof(&cpu), &cpu);
+    pthread_setaffinity_np(schedulerThread, sizeof(&cpu), &cpu); // Set processor affinity;
+    pthread_setaffinity_np(T1, sizeof(&cpu), &cpu); // Set processor affinity;
+    pthread_setaffinity_np(T2, sizeof(&cpu), &cpu); // Set processor affinity;
+    pthread_setaffinity_np(T3, sizeof(&cpu), &cpu); // Set processor affinity;
+    pthread_setaffinity_np(T4, sizeof(&cpu), &cpu); // Set processor affinity;
 
 
     param0.__sched_priority = 10; // DOUBLE CHECK THIS IF YOU RUN INTO TROUBLE
@@ -78,23 +159,12 @@ int main() {
     param3.__sched_priority = 7; // DOUBLE CHECK THIS IF YOU RUN INTO TROUBLE
     param4.__sched_priority = 6; // DOUBLE CHECK THIS IF YOU RUN INTO TROUBLE
 
+    //Temp var
+    int tempParam = 0;
 
-    schedulerThread = pthread_create(&schedulerThread, &attr0, PASS IN YOUR SCHEDULER FUNCTION HERE, PASS IN THE NEEDED ARGS);
-    schedulerThread = pthread_create(&T1, &attr1, PASS IN YOUR SCHEDULER FUNCTION HERE, PASS IN THE NEEDED ARGS);
-    schedulerThread = pthread_create(&T2, &attr2, PASS IN YOUR SCHEDULER FUNCTION HERE, PASS IN THE NEEDED ARGS);
-    schedulerThread = pthread_create(&T3, &attr3, PASS IN YOUR SCHEDULER FUNCTION HERE, PASS IN THE NEEDED ARGS);
-    schedulerThread = pthread_create(&T4, &attr4, PASS IN YOUR SCHEDULER FUNCTION HERE, PASS IN THE NEEDED ARGS);
+    int tidSchThr = pthread_create(&schedulerThread, &attr0, scheduler, &tempParam);
 
-
-    // Initialize do work matrix
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            doWorkMatrix[i][j] = 1; // Set all matrix values to one
-        }
-    }
-
-
-
+    /*
     // Now test ms clock values with Chrono
     auto time1 = chrono::high_resolution_clock::now();
     // Let's test a bunch
@@ -107,7 +177,7 @@ int main() {
     chrono::duration<double, milli> fms_conversion = (time2 - time1);
 
     cout << wms_conversion.count() << " whole seconds" << endl;
-    cout << fms_conversion.count() << " milliseconds" << endl;
+    cout << fms_conversion.count() << " milliseconds" << endl; */
 
     return 0;
 }
